7db2b6c972ca1bb2276a2843407a0f478b1591d4
Luca Scaringella <lucalstyle@gmail.com>
2020-12-08 12:00:19 +0100

    Implement Exchange class which can be connected to a broker.

---
+++ src/lib/Exchange.ts
@@ -0,0 +1,40 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import EventEmitter from "emitix";
+
+type PublishEmitter = EventEmitter<{[key: string]: [any,boolean]}>;
+
+export default class Exchange {
+
+    public readonly subscriptions: ReadonlyArray<string>;
+    public subscribe: (channel: string) => void;
+    public unsubscribe: (channel: string) => void;
+    public publish: (channel: string, data: any, processComplexTypes?: boolean) => void;
+
+    private readonly _publishEmitter: PublishEmitter = new EventEmitter();
+    public readonly oncePublish: PublishEmitter['once'] = this._publishEmitter.once.bind(this._publishEmitter);
+    public readonly onPublish: PublishEmitter['on'] = this._publishEmitter.on.bind(this._publishEmitter);
+    public readonly offPublish: PublishEmitter['off'] = this._publishEmitter.off.bind(this._publishEmitter);
+    /**
+     * @private
+     * @internal
+     */
+    public readonly _emitPublish: PublishEmitter['emit'] = this._publishEmitter.emit.bind(this._publishEmitter);
+
+    constructor(connector: {
+        subscriptions: string[],
+        subscribe: (channel: string) => void,
+        unsubscribe: (channel: string) => void,
+        publish: (channel: string, data: any, processComplexTypes?: boolean) => void;
+    }) {
+        this.subscriptions = connector.subscriptions;
+        this.subscribe = connector.subscribe;
+        this.unsubscribe = connector.unsubscribe;
+        this.publish = connector.publish;
+    }
+
+}
