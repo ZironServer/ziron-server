e5f33e5b7cd2318d60e8bcf14db46125491c50f1
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2021-12-16 11:37:35 +0100

    Add missing HTTP method properties.

--- src/lib/Http.ts
+++ src/lib/Http.ts
@@ -7,5 +7,5 @@ Copyright(c) Ing. Luca Gian Scaringella
 import {TemplatedApp} from "ziron-ws";
 import { ReplaceReturnType } from "./Utils";
 
-type HttpFunctions = Pick<TemplatedApp,"get" | "post" | "put" | "del" | "head" | "patch" | "options">;
+type HttpFunctions = Pick<TemplatedApp,"any" | "get" | "post" | "put" | "del" | "head" | "patch" | "options" | "connect" | "trace">;
 export type Http = {[K in keyof HttpFunctions]: ReplaceReturnType<HttpFunctions[K],void>}
